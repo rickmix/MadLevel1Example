3f9a4def4530132d806afd978719ef24bf2c80e1
Rick Schut <rick-schut@live.nl>
2020-09-02 10:17:08 +0200

    Mad level 1 example project

---
+++ app/src/main/java/com/example/madlevel1example/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.madlevel1example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.madlevel1example.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        binding.btnConfirm.setOnClickListener {
+            checkAnswer()
+        }
+    }
+
+    private fun checkAnswer() {
+        val answer = binding.etAnwser.text.toString();
+
+        if(answer == getString(R.string.dog)){
+            Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_LONG).show()
+        }
+    }
+}
